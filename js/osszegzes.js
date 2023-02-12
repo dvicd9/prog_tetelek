8c538bdd88718382a7f6a3f876e22bcce24c383c
Gregor <dvicd9@inf.elte.hu>
2023-02-12 14:40:25 +0100

    Osszegzes kesz

---
+++ js/osszegzes.js
@@ -0,0 +1,7 @@
+function osszegzes(){
+    let osszeg = 0;
+    for (let i = 0; i < tomb.length; i++){
+        osszeg += tomb[i];
+    }
+    document.getElementById("osszeg").innerHTML = osszeg;
+}
