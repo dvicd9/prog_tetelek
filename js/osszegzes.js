d615d240d67862f01da0f2139ef5036ec776f18e
Gregor <dvicd9@inf.elte.hu>
2023-02-12 14:48:04 +0100

    Osszegzes atirva, atlagolas kesz

--- js/osszegzes.js
+++ js/osszegzes.js
@@ -1,7 +1,16 @@
-function osszegzes(){
+function osszegzes(my_tomb){
     let osszeg = 0;
-    for (let i = 0; i < tomb.length; i++){
-        osszeg += tomb[i];
+    for (let i = 0; i < my_tomb.length; i++){
+        osszeg += my_tomb[i];
     }
     document.getElementById("osszeg").innerHTML = osszeg;
+}
+
+function atlagolas(my_tomb){
+    document.getElementById("atlag").innerHTML = osszegzes(my_tomb) / my_tomb.length;
+}
+
+function szamolas(){
+    osszegzes(tomb);
+    atlagolas(tomb);
 }
